ff3d46e0ce952b9d1e2aa1e83d54684a44d38772
Stoic <dolphinstark@protonmail.com>
2026-06-11 09:21:38 +0000

    Fix form hang: send as simple request (text/plain, no CORS preflight) + allow any origin

--- workers/stoic-mailer.js
+++ workers/stoic-mailer.js
@@ -1,5 +1,5 @@
 const CORS_HEADERS = {
-  'Access-Control-Allow-Origin': 'https://dolphinstark.com',
+  'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type',
 };
